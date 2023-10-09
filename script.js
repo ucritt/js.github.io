96cc31c0255820cef5719ee116c4c4ff3a7f68ec
ucrit <fajarseno9@gmail.com>
2023-10-10 06:31:48 +0700

    First

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+// Deklarasi variabel
+var x = 10;
+var y = 15;
+
+// Pertambahan
+var tambah = x + y;
+document.write("Hasil Pertambahan: " + tambah,"<br>");
+
+// Pengurangan
+var kurang = x - y;
+document.write("Hasil Pengurangan: " + kurang,"<br>");
+
+// Perkalian
+var kali = x * y;
+document.write("Hasil Perkalian: " + kali,"<br>");
+
+// Pembagian
+var bagi = x / y;
+document.write("Hasil Pembagian: " + bagi,"<br>");
+
+// Modulus
+var modulus = x % y;
+document.write("Hasil Modulus: " + modulus,"<br>");
